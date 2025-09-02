cfc07845dcd93fa113002bfb6cfc30c3ecc689a6
fariha39 <jahanfariha321@gmail.com>
2025-09-02 10:48:41 +0600

    delete user

--- src/test/java/RESTAssured.java
+++ src/test/java/RESTAssured.java
@@ -48,6 +48,34 @@ public class RESTAssured {
     String id =jsonobj.get("user.id").toString(); //config file sob data by default as string hisebei store hoi
     System.out.println(id);
 
+    }
+    @Test
+    public void createUser(){
+        RestAssured.baseURI=prop.getProperty("baseurl");
+        Response res=(Response) given().contentType("application/json")
+                .header("Authorization","bearer "+prop.getProperty("token"))
+                .header("X-AUTH-SECRET-KEY",prop.getProperty("secretkey"))
+                .body("{\n" +
+                        "  \"name\": \"user12357\",\n" +
+                        "  \"email\": \"user1234@gmail.com\",\n" +
+                        "  \"password\": \"12346\",\n" +
+                        "  \"phone_number\": \"01264752631\",\n" +
+                        "  \"nid\": \"7868732347845\",\n" +
+                        "  \"role\": \"Customer\"\n" +
+                        "\n" +
+                        "}")
+                .when().post("/user/create");
+        System.out.println(res.asString());
+    }
+    @Test
+    public void deleteUser() {
+        RestAssured.baseURI=prop.getProperty("baseurl");
+        Response res=given().contentType("application/json")
+                .header("Authorization","bearer "+prop.getProperty("token"))
+                .header("X-AUTH-SECRET-KEY",prop.getProperty("secretkey"))
+                .when().delete("/user/delete/88989");
+        System.out.println(res.asString());
+
 
     }
 }
